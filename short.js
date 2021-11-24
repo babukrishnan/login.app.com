bda996556f29036547b70729aa1976b4de00acc4
babukrishnan <90099438+babukrishnan@users.noreply.github.com>
2021-11-24 05:00:14 -0800

    Add files via upload

---
+++ short.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("sigin")
+var y = document.getElementById("login")
+var z = document.getElementById("btn")
+
+function login(){
+    x.style.left = "-350px"
+    y.style.left = "0px"
+    z.style.left = "100px"
+}
+
+function sigin(){
+    x.style.left = "0px"
+    y.style.left = "350px"
+    z.style.left = "0px"
+}
